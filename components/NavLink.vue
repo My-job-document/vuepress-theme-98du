7e3f411d73b16c50b491dff4148ac8ee772f04b6
zhuangpinghua <zhuangpinghua@98du.com>
2020-12-01 10:34:23 +0800

    update: 修改样式以及添加item.target属性.

--- components/NavLink.vue
+++ components/NavLink.vue
@@ -11,7 +11,7 @@
     v-else
     :href="link"
     class="nav-link external"
-    :target="isMailto(link) || isTel(link) ? null : '_blank'"
+    :target="item.target || isMailto(link) || isTel(link) ? null : '_blank'"
     :rel="isMailto(link) || isTel(link) ? null : 'noopener noreferrer'"
   >
     <i :class="`iconfont ${item.icon}`"></i>
